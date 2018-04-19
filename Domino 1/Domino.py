f48079252f00ede8fe23b708b553ae08da8fd59f
tobynwuba <tobynwuba@yahoo.com>
2018-04-19 17:40:06 +0100

    Added initial comment and variables that aid flexibility of the code generally

--- Domino 1/Domino.py
+++ Domino 1/Domino.py
@@ -1,7 +1,14 @@
-arr = [[0] * 6 for _ in xrange(6)]
+"""
+Written to work for both Domino 1 and 2
+"""
+
+
+length = 4
+arr = [[0] * length for _ in xrange(length)]
 gs = file('GameState.txt', 'r')
-target = [13]
-done = [False]
+global target, done
+target = 6
+done = False
 possible_moves = set([tuple(map(int, gs.readline().split())) for _ in xrange(28)])
 
 
@@ -9,8 +16,9 @@ def check():
     row = [0] * len(arr)
     diag1 = 0
     diag2 = 0
+    global target
     for i in xrange(len(arr)):
-        if sum(arr[i]) > target[0]:
+        if sum(arr[i]) > target:
             return False
         for j in xrange(len(arr)):
             row[j] += arr[i][j]
@@ -18,7 +26,7 @@ def check():
                 diag1 += arr[i][j]
             if i == len(arr) - 1 - j:
                 diag2 += arr[i][j]
-            if row[j] > target[0] or diag1 > target[0] or diag2 > target[0]:
+            if row[j] > target or diag1 > target or diag2 > target:
                 return False
     return True
 
@@ -27,8 +35,9 @@ def check_final():
     row = [0] * len(arr)
     diag1 = 0
     diag2 = 0
+    global target
     for i in xrange(len(arr)):
-        if sum(arr[i]) != target[0]:
+        if sum(arr[i]) != target:
             return False
         for j in xrange(len(arr)):
             row[j] += arr[i][j]
@@ -37,12 +46,13 @@ def check_final():
             if i == len(arr) - 1 - j:
                 diag2 += arr[i][j]
     for j in xrange(len(arr)):
-        if row[j] != target[0] or diag1 != target[0] or diag2 != target[0]:
+        if row[j] != target or diag1 != target or diag2 != target:
             return False
     return True
 
 
 def dfs(level=0):
+    global done
     for s in list(possible_moves):
         arr[level][0], arr[level + 1][0] = s
         possible_moves.remove(s)
@@ -50,32 +60,39 @@ def dfs(level=0):
         dfs_visit(level + 1)
         possible_moves.add(s)
         possible_moves.add(s[::-1])
-        if done[0]:
+        if done:
             return
 
 
 def dfs_visit(level):
-    if level > 17:
+    global done
+    if level > 7:
         return
-    elif level < 6:
+    elif level < 4:
         row, col = 0, level
-    elif level < 12:
-        row, col = 2, level - 6
     else:
-        row, col = 4, level - 12
+        row, col = 2, level - 4
+    # if level > 17:
+    #     return
+    # elif level < 6:
+    #     row, col = 0, level
+    # elif level < 12:
+    #     row, col = 2, level - 6
+    # else:
+    #     row, col = 4, level - 12
     for v in list(possible_moves):
         arr[row][col], arr[row + 1][col] = v
         possible_moves.discard(v)
         possible_moves.discard(v[::-1])
         correct = check()
-        if level == 17:
+        if level == 7:
             correct = check_final()
             if correct:
-                done[0] = True
+                done = True
                 return
         if correct:
             dfs_visit(level + 1)
-            if done[0]:
+            if done:
                 return
         arr[row][col], arr[row + 1][col] = [0, 0]
         possible_moves.add(v)
