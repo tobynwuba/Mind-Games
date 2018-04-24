d0609b3d3ce80137a59751f15b3f0f2c8791efbd
tobynwuba <tobynwuba@yahoo.com>
2018-04-24 05:58:37 +0100

    Brreak while adding a forward checking filter

--- Domino 1/Domino.py
+++ Domino 1/Domino.py
@@ -1,7 +1,7 @@
 """
 Written to work for both Domino 1 and 2
 """
-
+from random import sample
 
 length = 4
 arr = [[0] * length for _ in xrange(length)]
@@ -10,6 +10,30 @@ global target, done
 target = 6
 done = False
 possible_moves = set([tuple(map(int, gs.readline().split())) for _ in xrange(28)])
+
+
+def forward_checking(r, c):
+    global target
+    col = 0
+    for i in xrange(len(arr)):
+        col += arr[i][c]
+    row1 = sum(arr[r])
+    row2 = sum(arr[r+1])
+    if r == c:
+        row1 = max(row1, calculate_diagonal1())
+    elif r + 1 == c:
+        row2 = max(row2, calculate_diagonal1())
+    elif r == len(arr) - 1 - c:
+        row1 = max(row1, calculate_diagonal2())
+    elif r + 1 == len(arr) - 1 - c:
+        row2 = max(row2, calculate_diagonal2())
+    copy_moves = possible_moves.copy()
+    col, row1, row2 = target - col, target - row1, target - row2
+    for i in possible_moves:
+        if sum(i) > col or i[0] > row1 or i[1] > row2:
+            copy_moves.discard(i)
+            copy_moves.discard(i[::-1])
+    return list(copy_moves)
 
 
 def check():
@@ -29,6 +53,24 @@ def check():
             if row[j] > target or diag1 > target or diag2 > target:
                 return False
     return True
+
+
+def calculate_diagonal1():
+    diagonal = 0
+    for i in xrange(len(arr)):
+        for j in xrange(len(arr)):
+            if i == j:
+                diagonal += arr[i][j]
+    return diagonal
+
+
+def calculate_diagonal2():
+    diagonal = 0
+    for i in xrange(len(arr)):
+        for j in xrange(len(arr)):
+            if i == len(arr) - 1 - j:
+                diagonal += arr[i][j]
+    return diagonal
 
 
 def check_final():
@@ -53,8 +95,8 @@ def check_final():
 
 def dfs(level=0):
     global done
-    for s in list(possible_moves):
-        arr[level][0], arr[level + 1][0] = s
+    for s in sample(list(possible_moves), len(possible_moves)):
+        arr[0][0], arr[1][0] = s
         possible_moves.remove(s)
         possible_moves.discard(s[::-1])
         dfs_visit(level + 1)
@@ -80,20 +122,19 @@ def dfs_visit(level):
     #     row, col = 2, level - 6
     # else:
     #     row, col = 4, level - 12
-    for v in list(possible_moves):
+    iterate_moves = forward_checking(row, col)
+    for v in sample(iterate_moves, len(iterate_moves)):
         arr[row][col], arr[row + 1][col] = v
         possible_moves.discard(v)
         possible_moves.discard(v[::-1])
-        correct = check()
         if level == 7:
             correct = check_final()
             if correct:
                 done = True
                 return
-        if correct:
-            dfs_visit(level + 1)
-            if done:
-                return
+        dfs_visit(level + 1)
+        if done:
+            return
         arr[row][col], arr[row + 1][col] = [0, 0]
         possible_moves.add(v)
         possible_moves.add(v[::-1])
